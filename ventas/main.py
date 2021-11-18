de98543799d9e219e6b1d2853e0be32b0b2e8acf
felipe <feliperestrepomeneses@gmail.com>
2021-11-18 09:07:07 -0500

    agregamos funciones a nuestra reasginacion de la variable clients para agregar mas clientes

--- ventas/main.py
+++ ventas/main.py
@@ -1,6 +1,29 @@
 
 clients = 'pablo,ricardo,'
 
+
+def create_client(client_name):
+    global clients
+    
+    clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+    
+    print(list_clients)
+
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+
 if __name__ == '__main__':
-    clients += 'david',
-    print(clients)
+    list_clients()
+
+    create_client('David')
+
+    list_clients()
