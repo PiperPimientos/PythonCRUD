c4328a7ab4259e1e5fa8887a4bdb2193fc0d5694
felipe <feliperestrepomeneses@gmail.com>
2021-11-20 11:44:13 -0500

    agregamos un while loop a nuestra funcion para _get_client_name, que nos permite hacer un bucle hasta que el usuario ingrese un nombre o se salga con 'exit'

--- ventas/main.py
+++ ventas/main.py
@@ -1,3 +1,5 @@
+import sys
+
 
 clients = 'pablo,ricardo,'
 
@@ -27,6 +29,16 @@ def delete_client(client_name):
     else:
         print('Client is not in clients list')
 
+def search_client(client_name):
+    clients_list = clients.split(',')
+    
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+            
+
 def list_clients():
     global clients
     
@@ -45,10 +57,23 @@ def _print_welcome():
     print('What would you like to do today?')
     print('[C]reate client')
     print('[D]elete client')
+    print('[S]earch client')
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return client_name
 
 if __name__ == '__main__':
     _print_welcome()
@@ -68,5 +93,13 @@ if __name__ == '__main__':
         updated_client_name = input('What is the updated client name: ')
         update_client(client_name, updated_client_name)
         list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print('The client {client_name} is in the clients list')
+        else:
+            print('The client {client_name} is not in the clients list')
     else:
         print('Invalid command')
